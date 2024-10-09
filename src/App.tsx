18611cdfc0eebd28dde5b76c5018f97ab945eefe
Vesta-nna <anna.dominguez.anna@gmail.com>
2024-10-09 19:00:40 +0200

    testimonial card working

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+const App = () => (
+	<div className="w-full min-h-screen bg-gradient-to-br from-[#F9FAFB] to-[#D2D6DB] flex items-center justify-center">
+		<main className="bg-white max-w-[340px] p-6 rounded-lg flex flex-col gap-4 shadow">
+			<header className="flex gap-4">
+				<img
+					src="/img/profile-thumbnail.png"
+					alt="Sarah Dole profile"
+					className="w-12 h-12"
+				/>
+				<article className="">
+					<h1 className="font-semibold text-lg text-neutral-900">Sarah Dole</h1>
+					<h2 className="text-neutral-600 text-sm">@sarahdole</h2>
+				</article>
+			</header>
+			<article>
+				<p className="text-neutral-600">
+					I've been searching for high-quality abstract images for my design
+					projects, and I'm thrilled to have found this platform. The variety
+					and depth of creativity are astounding!
+				</p>
+			</article>
+		</main>
+	</div>
+);
+
+export default App;
